3bc33a1709ea394102a70982815375a78608ff92
Sangwon56 <sw000506@naver.com>
2024-05-31 21:50:38 +0900

    add mul, div
    add mul, div

---
+++ src/main/java/com/sbmavenweb/study01/MathExam/MathExam.java
@@ -0,0 +1,37 @@
+package com.sbmavenweb.study01.MathExam;
+
+
+public class MathExam {
+
+    public int subTest01(int n1, int n2) throws Exception {
+        if (n1 < -50000 || n1 > 50000) {
+            throw new Exception("num1값은 -50000보다 작거나 50000보다 클 수 없습니다.");
+        }
+        if (n2 < -50000 || n2 > 50000) {
+            throw new RuntimeException("num2값은 -50000보다 작거나 50000보다 클 수 없습니다.");
+        }
+        return n1 - n2;
+    }
+
+    public int mulTest01(int n1, int n2) throws Exception {
+        if (n1 <= 0 || n1 >= 100) {
+            throw new Exception("num1의 값은 0과 100의 사잇값이어야 합니다.");
+        }
+        if (n2 <= 0 || n2 >= 100) {
+            throw new Exception("num1의 값은 0과 100의 사잇값이어야 합니다.");
+        }
+        return n1 * n2;
+    }
+
+    public int divTest01(int n1, int n2) throws Exception {
+        if (n1 < 0 || n1 >= 100) {
+            throw new Exception("num1의 값은 0보다 크거나 100보다 작아야 합니다.");
+        }
+        if (n2 < 0 || n2 >= 100) {
+            throw new Exception("num2의 값은 0보다 크거나 100보다 작아야 합니다.");
+        }
+        return n1 / n2;
+    }
+
+
+}
--- src/test/java/com/sbmavenweb/study01/exam01Test.java
+++ src/test/java/com/sbmavenweb/study01/exam01Test.java
@@ -1,43 +1,69 @@
 package com.sbmavenweb.study01;
 
+import com.sbmavenweb.study01.MathExam.MathExam;
 import net.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class exam01Test {
     @Test
-    public void subtract01() throws Exception{
+    public void subtract01() throws Exception {
+        MathExam math = new MathExam();
         //given 테스트를 위한 준비값
         int num1 = 2;
         int num2 = 3;
         int expected = -1;
         //when 테스트를 실시
-        int actual = subTest01(num1, num2);
+        int actual = math.subTest01(num1, num2);
 
         //then 실제값과 기대값을 비교해서 테스트한다
         assertThat(actual).isEqualTo(expected);
-        assertThat(-1).isEqualTo(subTest01(2, 3));
-        assertThat(29).isLessThan(subTest01(100, 70));
-        assertThat(301).isGreaterThan(subTest01(1000, 700));
+        assertThat(-1).isEqualTo(math.subTest01(2, 3));
+        assertThat(29).isLessThan(math.subTest01(100, 70));
+        assertThat(301).isGreaterThan(math.subTest01(1000, 700));
 
         //Exception 예외가 발생해야지만 정상 동작으로 인식한다.
-        assertThatThrownBy(() -> subTest01(70000, 7))
+        assertThatThrownBy(() -> math.subTest01(70000, 7))
                 .isInstanceOf(Exception.class);
         //RuntimeException 예외가 발생해야지만 정상 동작으로 인식한다.
-        Throwable exception  = assertThrows(RuntimeException.class, () -> {
-            subTest01(2330, 50002);
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            math.subTest01(2330, 50002);
         });
         System.out.println(exception.toString());
     }
-    int subTest01(int n1, int n2) throws Exception{
-        if( n1 < -50000 || n1 > 50000) {
-            throw new Exception("num1값은 -50000보다 작거나 50000보다 클 수 없습니다.");
-        }
-        if( n2 < -50000 || n2 > 50000) {
-            throw new RuntimeException("num2값은 -50000보다 작거나 50000보다 클 수 없습니다.");
-        }
-        return n1 - n2;
+
+    @Test
+    public void multiply01() throws Exception {
+        // given
+        MathExam math = new MathExam();
+
+        // when
+        // then
+        assertThat(12).isEqualTo(math.mulTest01(3, 4));
+        assertThat(513).isEqualTo(math.mulTest01(27, 19));
+        assertThatThrownBy(() -> math.mulTest01(-1, 10))
+                .isInstanceOf(Exception.class);
+        assertThatThrownBy(() -> math.mulTest01(10, 101))
+                .isInstanceOf(Exception.class);
+
     }
+
+    @Test
+    public void divide01() throws Exception {
+        //given
+        MathExam math = new MathExam();
+        //when
+        //then
+        assertThat(2).isEqualTo(math.divTest01(10, 5));
+        assertThat(3).isEqualTo(math.divTest01(7, 2));
+        assertThatThrownBy(() -> math.divTest01(-1, 10))
+                .isInstanceOf(Exception.class);
+        assertThatThrownBy(() -> math.divTest01(10, 101))
+                .isInstanceOf(Exception.class);
+    }
+
+
 }
