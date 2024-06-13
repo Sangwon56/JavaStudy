5d6573f642737182f3ae33c76aa70d319700f137
Sangwon56 <sw000506@naver.com>
2024-06-13 20:06:30 +0900

    교재 19번 문제

--- src/main/java/com/sbmavenweb/study01/MathExam/Main.java
+++ src/main/java/com/sbmavenweb/study01/MathExam/Main.java
@@ -1,11 +1,33 @@
 package com.sbmavenweb.study01.MathExam;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Arrays;
 import java.util.Scanner;
-
+@Getter
+@Setter
 public class Main {
+    final int MAX_BALANCE = 0;
+    final int MIN_BALANCE = 0;
+    private int balance;
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        if ( balance < 0 || balance > 1000000) {
+            return;
+        } else {
+            this.balance = balance;
+        }
+    }
     public static void main(String args[]) {
-        Scanner x = new Scanner(System.in);
+            Main a = new Main();
 
+            a.setBalance(10000);
+            System.out.println("현재 잔고 : " + a.getBalance());
+
+            a.setBalance(-100);
+            System.out.println("현재 잔고 : " + a.getBalance());
     }
 }
